ef704b9f6bd95a0d607800a1aa4853db34ab84b5
Ivan Harmat <ivanharmat@yahoo.com>
2013-11-25 01:06:56 -0500

    Added Index File

---
+++ application/modules/admin/views/index.php
@@ -0,0 +1,6 @@
+<form class="form-signin">
+	<h2 class="form-signin-heading">Please sign in</h2>
+	<input type="text" class="form-control" placeholder="Email address" required autofocus>
+	<input type="password" class="form-control" placeholder="Password" required>
+	<button class="btn btn-lg btn-custom-gharbieh btn-block" type="submit"><i class="fa fa-lock"></i> Sign in</button>
+</form>
